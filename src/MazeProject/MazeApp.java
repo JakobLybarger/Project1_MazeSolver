4471b31e14c82d8d9b2a86272e3996413fc4f59d
Jakob Lybarger <jakoblybarger911@gmail.com>
2021-10-13 20:12:44 -0500

    Thing works

--- src/MazeProject/MazeApp.java
+++ src/MazeProject/MazeApp.java
@@ -4,39 +4,74 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class MazeApp {
+public class MazeApp extends JFrame {
 
-    static MazeReader mazeReader;
-    static MazeSolver mazeSolver;
-    static JFrame frame = new JFrame("Maze Solver");
+    private char[][] maze;
+    private boolean loaded = false;
+    private boolean started = false;
+    private boolean running = false;
+    private static MazeReader mazeReader;
+    private static MazeSolver mazeSolver;
+    private ArrayList<Integer> path = new ArrayList<>();
+    private int pathIndex;
+    private boolean solutionFound;
+    Timer timer;
 
+    public MazeApp() {
+        setTitle("Levi, Jakob, and Sandeep's Maze Solver");
+        setSize(640, 480);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    public static void main(String[] args) {
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 800);
-
-        // text field, load, start
-        JPanel panel = new JPanel();
+        JPanel topPanel = new JPanel();
         JLabel label = new JLabel("File Name: ");
         JTextField textField = new JTextField(20);
         JButton load = new JButton("Load");
         JButton start = new JButton("Start");
-        panel.add(label);
-        panel.add(textField);
-        panel.add(load);
-        panel.add(start);
+        JButton step = new JButton("Step");
+        JButton animate = new JButton("Animate");
 
+        JPanel bottomPanel = new JPanel();
+        JLabel status = new JLabel("No maze");
+        JLabel algorithm = new JLabel("");
+
+
+        step.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                pathIndex += 2;
+
+                if (pathIndex > path.size() - 2) {
+                    pathIndex = path.size() - 2;
+                    if (solutionFound) {
+                        status.setText("Solution complete: exit reachable at "
+                                + path.get(path.size()-2) + "," + path.get(path.size()-1)
+                                + " in " + path.size() / 2 + " moves.");
+                    } else {
+                        status.setText("Solution complete: exit not reachable");
+                    }
+                    timer.stop();
+                    running = false;
+                }
+                repaint();
+            }
+        });
         load.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
                     mazeReader = new MazeReader(textField.getText());
-                    System.out.println(mazeReader);
-
-                    temporarything();
-                    SwingUtilities.updateComponentTreeUI(frame);
+                    maze = mazeReader.getMaze();
+                    mazeSolver = new MazeSolver(maze);
+                    status.setText("Maze Loaded");
+                    algorithm.setText("");
+                    loaded = true;
+                    started = false;
+                    pathIndex = -1;
+                    path = new ArrayList<>();
+                    repaint();
                 } catch (Error e) {
                     System.err.println("Invalid file name try again");
                     System.err.println(e.getStackTrace());
@@ -46,48 +81,127 @@ public class MazeApp {
         });
 
         start.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                try {
-                    mazeSolver = new MazeSolver(mazeReader.getMaze());
-//                    mazeSolver.GBFS();
-//                    System.out.println("Length of path: " + mazeSolver.GBFS());
-                } catch (Error e) {
-                    System.err.println("No file loaded");
-                    System.err.println(e.getStackTrace());
+                String selectedAlgorithm;
+                if (loaded && !started) {
+                    started = true;
+                    status.setText("Solution in progress");
+                    ArrayList<Integer> bfsPath = new ArrayList<>();
+                    ArrayList<Integer> dfsPath = new ArrayList<>();
+                    ArrayList<Integer> gbfsPath = new ArrayList<>();
+                    mazeSolver.BFS(bfsPath);
+                    mazeSolver.DFS(dfsPath);
+                    solutionFound = mazeSolver.GBFS(gbfsPath);
+
+                    path = gbfsPath;
+                    selectedAlgorithm = "   Greedy Best-First Search";
+                    if (dfsPath.size() < path.size()) {
+                        path = dfsPath;c
+                        selectedAlgorithm = "   Depth-First Search";
+                    }
+                    if (bfsPath.size() < path.size()) {
+                        path = bfsPath;
+                        selectedAlgorithm = "   Breadth-First Search";
+                    }
+                    algorithm.setText(selectedAlgorithm);
                 }
             }
         });
 
-        frame.getContentPane().add(BorderLayout.NORTH, panel);
-        frame.setVisible(true);
+        timer = new Timer(500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                step.doClick();
+            }
+        });
+        animate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                running = !running;
+                if (running) {
+                    timer.start();
+                } else {
+                    timer.stop();
+                }
+            }
+        });
+
+
+
+        topPanel.add(label);
+        topPanel.add(textField);
+        topPanel.add(load);
+        topPanel.add(start);
+        topPanel.add(step);
+        topPanel.add(animate);
+
+        bottomPanel.add(status);
+        bottomPanel.add(algorithm);
+
+        add(BorderLayout.NORTH, topPanel);
+        add(BorderLayout.SOUTH, bottomPanel);
+
+        pathIndex = -1;
     }
 
-    static private void temporarything() {
-        char[][] charMaze = mazeReader.getMaze();
-        int rows = mazeReader.getMaze().length, cols = mazeReader.getMaze()[0].length;
-        JPanel mazePanel = new JPanel();
-        mazePanel.setLayout(new GridLayout(rows, cols));
-        JButton[][] maze = new JButton[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                maze[i][j] = new JButton();
-                maze[i][j].setPreferredSize(new Dimension(30, 30));
-                if (charMaze[i][j] == '.') {
-                    maze[i][j].setBackground(Color.white);
-                } else if (charMaze[i][j] == '#') {
-                    maze[i][j].setBackground(Color.black);
-                } else if (charMaze[i][j] == 'o') {
-                    maze[i][j].setText("Start");
-                    maze[i][j].setBackground(Color.green);
-                } else {
-                    maze[i][j].setText("Goal");
-                    maze[i][j].setBackground(Color.red);
-                }
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
 
-                mazePanel.add(maze[i][j]);
+        if (loaded) {
+            g.translate(0, 70);
+
+            int frameHeight = getBounds().getSize().height-95;
+            int frameWidth = getBounds().getSize().width;
+            int mazeRows = maze.length;
+            int mazeCols = maze[0].length;
+            int cellHeight = frameHeight / mazeRows;
+            int cellWidth = frameWidth / mazeCols;
+
+            // Can be used to keep aspect ratio
+//            if (cellHeight < cellWidth) {
+//                cellWidth = cellHeight;
+//            } else {
+//                cellHeight = cellWidth;
+//            }
+
+            for (int row = 0; row < maze.length; row++) {
+                for (int col = 0; col < maze[0].length; col++) {
+                    Color color;
+                    switch(maze[row][col]) {
+                        case '#': color = Color.BLACK; break;
+                        case '.': color = Color.WHITE; break;
+                        case '*': color = Color.RED; break;
+                        case 'o': color = Color.GREEN; break;
+                        default:
+                            throw new IllegalStateException("Unexpected value: " + maze[row][col]);
+                    }
+                    g.setColor(color);
+                    g.fillRect(cellWidth * col, cellHeight * row, cellWidth, cellHeight);
+                    g.setColor(Color.BLACK);
+                    g.drawRect(cellWidth * col, cellHeight * row, cellWidth, cellHeight);
+                }
+            }
+
+            for (int p = 0; p <= pathIndex-2; p += 2) {
+                int pathX = path.get(p);
+                int pathY = path.get(p+1);
+                g.setColor(Color.GRAY);
+                g.fillRect(pathX * cellWidth, pathY * cellHeight, cellWidth, cellHeight);
+                g.setColor(Color.BLACK);
+                g.drawRect(cellWidth * pathX, cellHeight * pathY, cellWidth, cellHeight);
             }
         }
+    }
 
-        frame.getContentPane().add(BorderLayout.CENTER, mazePanel);
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MazeApp mazeApp = new MazeApp();
+                mazeApp.setVisible(true);
+            }
+        });
     }
 }
--- src/MazeProject/Tutorial_View.java
+++ src/MazeProject/Tutorial_View.java
@@ -4,109 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.math.*;
-//
-//public class Tutorial_View extends JFrame {
-//    private static MazeReader mazeReader;
-//    private final List<Integer> path = new ArrayList<>();
-//    private int pathIndex;
-//    private boolean loaded;
-//
-//    public Tutorial_View() {
-//        setTitle("Simple Maze Solver");
-//        setSize(800, 800);
-//        setLocationRelativeTo(null);
-//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//        JPanel panel = new JPanel();
-//        JLabel label = new JLabel("File Name: ");
-//        JTextField textField = new JTextField(20);
-//        JButton load = new JButton("Load");
-//        JButton start = new JButton("Start");
-//        panel.add(label);
-//        panel.add(textField);
-//        panel.add(load);
-//        panel.add(start);
-//
-//        load.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent actionEvent) {
-//                try {
-//                    mazeReader = new MazeReader(textField.getText());
-//                    System.out.println(mazeReader);
-//                    loaded = true;
-////                    temporarything();
-//                    repaint();
-//                } catch (Error e) {
-//                    System.err.println("Invalid file name try again");
-//                    System.err.println(e.getStackTrace());
-//                }
-//
-//            }
-//        });
-//
-//        start.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent actionEvent) {
-//                try {
-//                    Tutorial_Solver.dfs(mazeReader.getMaze(), path);
-//
-//                } catch (Error e) {
-//                    System.err.println("No file loaded");
-//                    System.err.println(e.getStackTrace());
-//                }
-//            }
-//        });
-//
-//        add(BorderLayout.NORTH, panel);
-//    }
-//
-//    @Override
-//    public void paint(Graphics g) {
-//        super.paint(g);
-//        if (loaded) {
-//            System.out.println("here");
-//            char[][] charMaze = mazeReader.getMaze();
-//            int rows = mazeReader.getMaze().length, cols = mazeReader.getMaze()[0].length;
-//            JPanel mazePanel = new JPanel();
-//            mazePanel.setLayout(new GridLayout(rows, cols));
-//            JButton[][] maze = new JButton[rows][cols];
-//            for (int i = 0; i < rows; i++) {
-//                for (int j = 0; j < cols; j++) {
-//                    maze[i][j] = new JButton();
-//                    maze[i][j].setPreferredSize(new Dimension(30, 30));
-//                    switch (charMaze[i][j]) {
-//                        case '.': maze[i][j].setBackground(Color.WHITE); break;
-//                        case '#': maze[i][j].setBackground(Color.BLACK); break;
-//                        case 'o': maze[i][j].setBackground(Color.GREEN); break;
-//                        case '*': maze[i][j].setBackground(Color.RED); break;
-//                        default:
-//                            throw new IllegalStateException("Unexpected Value: " + maze[i][j]);
-//                    }
-//                    mazePanel.add(maze[i][j]);
-//                    System.out.println("Adding");
-//                }
-//            }
-//
-//            add(BorderLayout.CENTER, mazePanel);
-//        }
-//
-//    }
-//
-//    public static void main(String[] args) {
-//        SwingUtilities.invokeLater(new Runnable() {
-//            @Override
-//            public void run() {
-//                Tutorial_View view = new Tutorial_View();
-//                view.setVisible(true);
-//            }
-//        });
-//    }
-//}
+
+
 
 public class Tutorial_View extends JFrame {
 
@@ -118,7 +19,6 @@ public class Tutorial_View extends JFrame {
     private static MazeSolver mazeSolver;
     private List<Integer> path = new ArrayList<>();
     private int pathIndex;
-    private String solutionStatus = "No maze";
     private boolean solutionFound;
     Timer timer;
 
@@ -139,7 +39,7 @@ public class Tutorial_View extends JFrame {
         JButton animate = new JButton("Animate");
 
         JPanel bottomPanel = new JPanel();
-        JLabel status = new JLabel(solutionStatus);
+        JLabel status = new JLabel("No maze");
 
 
         step.addActionListener(new ActionListener() {
@@ -148,18 +48,14 @@ public class Tutorial_View extends JFrame {
                 pathIndex += 2;
                 System.out.println(pathIndex + " + " + path.size());
 
-//                System.out.println(solutionFound);
                 if (pathIndex > path.size() - 2) {
-                    System.out.println("In here");
                     pathIndex = path.size() - 2;
                     if (solutionFound) {
-                        solutionStatus = "Solution complete: exit reachable at "
+                        status.setText("Solution complete: exit reachable at "
                                 + path.get(path.size()-2) + "," + path.get(path.size()-1)
-                                + " in " + path.size() / 2 + " moves.";
-                        status.setText(solutionStatus);
+                                + " in " + path.size() / 2 + " moves.");
                     } else {
-                        solutionStatus = "Solution complete: exit not reachable";
-                        status.setText(solutionStatus);
+                        status.setText("Solution complete: exit not reachable");
                     }
                     timer.stop();
                 }
@@ -173,15 +69,12 @@ public class Tutorial_View extends JFrame {
                     mazeReader = new MazeReader(textField.getText());
                     maze = mazeReader.getMaze();
                     mazeSolver = new MazeSolver(maze);
-                    solutionStatus = "Maze Loaded";
-                    status.setText(solutionStatus);
-                    System.out.println(mazeReader);
+                    status.setText("Maze Loaded");
                     loaded = true;
                     started = false;
                     pathIndex = -1;
                     path = new ArrayList<>();
                     repaint();
-//                    temporarything();
                 } catch (Error e) {
                     System.err.println("Invalid file name try again");
                     System.err.println(e.getStackTrace());
@@ -195,8 +88,8 @@ public class Tutorial_View extends JFrame {
             public void actionPerformed(ActionEvent actionEvent) {
                 if (loaded && !started) {
                     started = true;
-                    solutionStatus = "Solution in progress";
-                    status.setText(solutionStatus);
+//                    solutionStatus = "Solution in progress";
+                    status.setText("Solution in progress");
                     solutionFound = mazeSolver.GBFS(path);
                 }
             }
@@ -234,7 +127,6 @@ public class Tutorial_View extends JFrame {
         add(BorderLayout.NORTH, topPanel);
         add(BorderLayout.SOUTH, bottomPanel);
 
-//        Tutorial_Solver.dfs(maze, path);
         pathIndex = -1;
     }
 
